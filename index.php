0193612bef3b87809378d10318c34cdd49873c89
ArturCarlos <arturcarlossantiago@gmail.com>
2019-06-13 13:58:32 -0300

    Páginas php

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<?php include "pages/juros-simples.php";
+
+?>
+
+<html>
+<head>
+    <title>Calculadora Finaceira</title>
+    <meta charset = "UTF-8">
+</head>
+<body>
+
+
+<h1>Calculadora Finaceira</h1>
+
+<form action="" method="post" >
+
+    <input type="submit" name="operacao" value="desconto-composto">
+    <input type="submit" name="operacao" value="desconto-simples">
+    <input type="submit" name="operacao" value="financiamento">
+    <input type="submit" name="operacao" value="juros-composto">
+    <input type="submit" name="operacao" value="juros-simples">
+    <input type="submit" name="operacao" value="montante">
+
+</form>
+
+<?php /*calculos();*/?>
+
+
+</body>
+</html>
