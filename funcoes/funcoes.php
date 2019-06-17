c552f498cc0d67932d5ad79fd7bc759b7f14a0b6
ArturCarlos <arturcarlossantiago@gmail.com>
2019-06-17 16:59:30 -0300

    Páginas juros simples

--- funcoes/funcoes.php
+++ funcoes/funcoes.php
@@ -29,11 +29,107 @@ function decontoSimples()
     if (isset($_GET['valDesc'])) {
         echo $_GET['valDesc'];
     }
-    /*nominal
-    taxa
-    taxa-t
-    tempo
-    tempo-dia
-    valDesc
-    */
+}
+
+function jurosSimples()
+{
+
+    if (isset($_GET['vp'])) {
+        $vp = $_GET['vp'];
+    } else {
+        $vp = null;
+    }
+    if (isset($_GET['taxa'])) {
+        $taxa = $_GET['taxa'];
+    } else {
+        $taxa = null;
+    }
+    if (isset($_GET['taxa-t'])) {
+        $taxa_t = $_GET['taxa-t'];
+    } else {
+        $taxa_t = null;
+    }
+    if (isset($_GET['tempo'])) {
+        $tempo = $_GET['tempo'];
+    } else {
+        $tempo = null;
+    }
+    if (isset($_GET['tempo-t'])) {
+        $tempo_t = $_GET['tempo-t'];
+    } else {
+        $tempo_t = null;
+    }
+    if (isset($_GET['vf'])) {
+        $vf = $_GET['vf'];
+    } else {
+        $vf = null;
+    }
+
+    $taxa = converteTaxa($taxa, $taxa_t);
+
+    $tempo = converteTempo($tempo, $tempo_t);
+
+    echo "<h4>Valor Presente: " . $vp . "</h4> ";
+    echo "<h4>Taxa (" . $taxa_t . "): " . $taxa . "</h4> ";
+    echo "<h4>Tempo (" . $tempo_t . "): " . $tempo . "</h4> ";
+
+
+    echo "<h3>Valor Futuro: " . jurosSimplesM($vp, $taxa, $tempo) . "</h3> ";
+
+}
+
+function jurosSimplesM($c, $i, $t)
+{
+
+    /*M = C.(1 + i.t)*/
+
+    $m = ($c * (1 + ($i * $t)));
+
+    return $m;
+}
+
+
+function converteTempo($val, $per)
+{
+
+    /*switch ($per) {
+        case 'dia':
+            $val = ($val / 30);
+            break;
+        case 'mes':
+            break;
+        case 'ano':
+            $val = $val * 12;
+            break;
+        case 'sem':
+            $val = $val * 6;
+            break;
+        case 'tri':
+            $val = $val * 4;
+            break;
+    }*/
+
+    return $val;
+}
+
+function converteTaxa($val, $i)
+{
+   /* switch ($i) {
+        case 'dia':
+            $val = $val / 30;
+            break;
+        case 'mes':
+            break;
+        case 'ano':
+            $val = $val * 12;
+            break;
+        case 'sem':
+            $val = $val * 6;
+            break;
+        case 'tri':
+            $val = $val * 4;
+            break;
+    }*/
+
+    return ($val/100);
 }
--- pages/juros-simples.php
+++ pages/juros-simples.php
@@ -1,65 +1,87 @@
 <?php require_once '../config.php'; ?>
 
-<?php include(FUNCOES); ?>
+<?php include(FUNCOES);
+include "modal-jur-simples.php";
+?>
 <?php include(HEADER_TEMPLATE); ?>
 
-<!--Fomulario e retorna o calcula da funcao-->
-<div class="container" style="margin-top: 20px;">
-    <h2>Juros Simples</h2>
-    <form action="/juros-simples.php">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="row" style="margin-top: 20px;">
-                    <div class="col-sm-12">
-                        <label for="vp">Valor Presente:</label>
-                        <input type="vp" class="form-control" id="vp" />
-                    </div>
-                </div>
-                <div class="row" style="margin-top: 20px;">
-                    <div class="col-sm-12">
-                        <label for="taxa">Taxa:</label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-6"><input type="number" class="form-control" id="taxa" /></div>
-                    <div class="col-sm-1"> % ao </div>
+    <!--Fomulario e retorna o calcula da funcao-->
+    <div class="container" style="margin-top: 20px;">
+        <h2>Juros Simples</h2>
+        <form action="juros-simples.php">
+            <div class="row">
+                <div class="col-sm-12">
 
-                    <div class="col-sm-5">
-                        <select class="form-control" id="taxa-t">
-                            <option>Dia</option>
-                            <option>Mês</option>
-                            <option>Ano</option>
-                            <option>Semestre</option>
-                            <option>Trimeste</option>
-                        </select>
+                    <div class="row" style="margin-top: 20px;">
+                        <div class="col-sm-12">
+                            <label for="vp">Valor Presente:</label>
+                            <input type="number" class="form-control" id="vp" name="vp"/>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-12" style="margin-top: 20px;">
-                        <label for="tempo">Tempo:</label>
+                    <div class="row" style="margin-top: 20px;">
+                        <div class="col-sm-12">
+                            <label for="taxa">Taxa:</label>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-6"><input type="number" class="form-control" id="tempo" /></div>
-                    <div class="col-sm-6">
-                        <select class="form-control" id="tempo-dia">
-                            <option>Dia</option>
-                            <option>Mês</option>
-                            <option>Ano</option>
-                            <option>Semestre</option>
-                            <option>Trimeste</option>
-                        </select>
+                    <div class="row">
+                        <div class="col-sm-6"><input type="number" class="form-control" id="taxa" name="taxa"/></div>
+                        <div class="col-sm-1"> % ao</div>
+
+                        <div class="col-sm-5">
+                            <select class="form-control" id="taxa-t" name="taxa-t">
+                                <option value="dia">Dia</option>
+                                <option value="mes">Mês</option>
+                                <option value="ano">Ano</option>
+                                <option value="sem">Semestre</option>
+                                <option value="tri">Trimeste</option>
+                            </select>
+                        </div>
                     </div>
-                </div>
-                <div class="row" style="margin-top: 20px;">
-                    <div class="col-sm-12">
-                        <label for="vf">Valor Futuro:</label>
-                        <input type="number" class="form-control" id="vf" />
+                    <div class="row">
+                        <div class="col-sm-12" style="margin-top: 20px;">
+                            <label for="tempo">Tempo:</label>
+                        </div>
                     </div>
+                    <div class="row">
+                        <div class="col-sm-6"><input type="number" class="form-control" id="tempo" name="tempo"/>
+                        </div>
+                        <div class="col-sm-6">
+                            <select class="form-control" id="tempo-dia" name="tempo-t">
+                                <option value="dia">Dia</option>
+                                <option value="mes">Mês</option>
+                                <option value="ano">Ano</option>
+                                <option value="sem">Semestre</option>
+                                <option value="tri">Trimeste</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row" style="margin-top: 20px;">
+                        <div class="col-sm-12">
+                            <label for="vf">Valor Futuro:</label>
+                            <input type="number" class="form-control" name="vf" id="vf"/>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block" style="margin-top: 20px;">Calcular</button>
+                    <input class="btn btn-outline-primary btn-block" style="margin-top: 20px;" type="reset"
+                           value="Limpar">
                 </div>
-                <button type="submit" class="btn btn-primary btn-block" style="margin-top: 20px;">Calcular</button>
-                <input class="btn btn-outline-primary btn-block" style="margin-top: 20px;" type="reset" value="Limpar">
+        </form>
+
+
+    </div>
+
+    <div class="box-header text-left">
+        <hr>
+        <h3>Resultado</h3>
+        <hr>
+
+
+        <div class="row" style="margin-top: 20px;">
+            <div class="col-sm-12">
+                <h4><?php jurosSimples();?></h4>
             </div>
-    </form>
-</div>
+        </div>
+
+    </div>
+
 <?php include(FOOTER_TEMPLATE); ?>
